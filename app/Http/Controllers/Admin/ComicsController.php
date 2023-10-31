cfc25f9a7da0dd6536d3381dfb9c2c1b85fe7766
Fabio Costantino <fabiocostantino84@gmail.com>
2023-10-31 16:33:08 +0100

    add ComicsControler

--- app/Http/Controllers/Admin/ComicsController.php
+++ app/Http/Controllers/Admin/ComicsController.php
@@ -1,9 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
 use App\Models\Comics;
 use Illuminate\Http\Request;
+use App\http\Controllers\Controller;
 
 class ComicsController extends Controller
 {
