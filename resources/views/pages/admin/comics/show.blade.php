7c523b2616cadf17b4b6f6ef6b313d56c220e299
Fabio Costantino <fabiocostantino84@gmail.com>
2023-11-06 18:14:40 +0100

    add link button torna indietro

--- resources/views/pages/admin/comics/show.blade.php
+++ resources/views/pages/admin/comics/show.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.welcome')
 
 @section('main_content')
-    
     {{-- se l'immagine la mette l'utente stmpo questo sotto, senò uso il percorso di prima --}}
     {{-- @if (Str::startsWith($comic->thumb, 'comics_images'))
         <img class="card-img-top" src="{{  asset('storage/' .  $comic->thumb)}}" alt="">
@@ -12,28 +11,34 @@
     @endif --}}
 
     <div class="container">
-        <img class="card-img-top" src="{{ Str::startsWith($comic->thumb, 'comics_images') ? asset('storage/' .  $comic->thumb) :  $comic->thumb }}" alt="">
+
+        <h1>{{ $comic->title }}</h1>
+
+        <a class="btn btn-secondary text-white my-5 " href="{{ route('comics.index') }}" role="button">Torna indietro</a>
+
+
+        <img class="card-img-top"
+            src="{{ Str::startsWith($comic->thumb, 'comics_images') ? asset('storage/' . $comic->thumb) : $comic->thumb }}"
+            alt="">
 
         <div class="card-body">
             <h5>Titolo:</h5>
-            <div>{{$comic->title}}</div>
+            <div>{{ $comic->title }}</div>
             <h5>Descrizione:</h5>
-            <div>{{$comic->description}}</div>
+            <div>{{ $comic->description }}</div>
             <h5>Prezzo:</h5>
-            <div>{{$comic->price}}</div>
+            <div>{{ $comic->price }}</div>
             <h5>Serie:</h5>
-            <div>{{$comic->series}}</div>
+            <div>{{ $comic->series }}</div>
             <h5>Data di uscita:</h5>
-            <div>{{$comic->sale_date}}</div>
+            <div>{{ $comic->sale_date }}</div>
             <h5>Genere:</h5>
-            <div>{{$comic->type}}</div>
+            <div>{{ $comic->type }}</div>
             <h5>Artista/i:</h5>
-            <div>{{$comic->artists}}</div>
+            <div>{{ $comic->artists }}</div>
             <h5>Autore:</h5>
-            <div>{{$comic->writers}}</div>
+            <div>{{ $comic->writers }}</div>
         </div>
-    </a>
 
     </div>
-
 @endsection
